6a0412d0f1e4196bcf3ae4e16fbb2be0f5f96cf1
Mushrit Shabnam <mushritshabnam@gmail.com>
2016-06-24 23:01:16 +0600

    linear search is added

--- chapter4/linear.cpp
+++ chapter4/linear.cpp
@@ -2,16 +2,33 @@
 #include<math.h>
 
 int LINEAR(int ARRAY[] , int LB , int UB , int ITEM , int LOC){
+    int a,flag = 0;
+    for(a=LB;a<=UB;a++){
+            if(ARRAY[a]==ITEM){
+                flag++;
+                printf("LOC =  %d\n", a);
+                return 0;
+            }
+    }
+    if(flag==0){
+        printf("LOC = 0\n");
+    }
 
 }
 
 int main(){
-    int i,j,k,l,n;
+    int i,j,k,l,n,m;
+    printf("enter the length of array: \n");
     scanf("%d", &n);
     int arr[n];
+    printf("enter the numbers of array: \n");
     for(i=0;i<n;i++){
       scanf("%d", &arr[i]);
     }
+    printf("enter the item to find: \n");
+    scanf("%d", &k);
+    printf("enter lower bound and upper bound:\n");
+    scanf("%d %d", &j,&m);
     LINEAR(arr,j,n,k,l);
     return 0;
 }
