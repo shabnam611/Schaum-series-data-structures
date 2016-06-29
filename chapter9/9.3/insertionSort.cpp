971632d9e96fc0e90a0be793a9a947d5280c73b4
Mushrit Shabnam <mushritshabnam@gmail.com>
2016-06-29 15:07:54 +0600

    insertion sort is done for chapter 9

--- chapter9/9.3/insertionSort.cpp
+++ chapter9/9.3/insertionSort.cpp
@@ -1,21 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
-int INSERTCOUNT(int A[] , int N , int NUMB){
-    int maxi = 0 ;
-    int k,temp,l,d,c;
+int INSERTCOUNT(int A[] , int N , int NUMB = 0){
+    int k,temp,l,j , c ;
     for(l=0;l<N;l++){
-
+            if((l==0)&&(A[l+1]<A[l])){
+               NUMB++;
+               temp = A[l];
+               A[l] = A[l+1];
+               A[l+1] = temp;
+               printf("A[l] = %d , A[l-1] = %d\n", A[l],A[l+1]);
+            }
+            else if((l>0)&&(A[l]<A[l-1])){
+                    NUMB++;
+                    j=l;
+                    k=j-1;
+                while((k>=0)&&(j>=1)){
+                    if(A[j]<A[k]){
+                       temp = A[k];
+                       A[k] = A[j];
+                       A[j] = temp;
+                    }
+                    k--;
+                    j--;
+                    printf("A[j] = %d , A[k] = %d , k = %d , j = %d\n", A[j],A[k],k,j);
+                }
+                j=0;
+                k=0;
+            }
     }
 
-    for(k=1;k<=(N-1);k++){
-        printf("%d", A[k]);
-    }
+            printf("the sorted array will be : \n");
+            for(c=0;c<N;c++){
+               printf("%d ", A[c]);
+            }
+            printf("\n");
+            printf("the number of comparisons are: %d\n", NUMB);
+            return 0;
 }
 int main(){
     int i,j,com = 0;
+    printf("enter the length of the array: \n");
     scanf("%d", &i);
     int arr[i];
-    for(j=1;j<=(i-1);j++){
+    printf("enter the elements of the array: \n");
+    for(j=0;j<i;j++){
         scanf("%d", &arr[j]);
     }
     INSERTCOUNT(arr,i,com);
